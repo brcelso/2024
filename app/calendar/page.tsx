a70a479314525a2338b54f5ac450a4bf4fcb3f3e
brcelso <129978598+brcelso@users.noreply.github.com>
2023-04-30 22:36:05 +0000

    ook

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -4,17 +4,16 @@ import * as React from "react"
 
 import { Calendar } from "@/components/ui/calendar"
 
-export function CalendarDemo() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
- 
-  return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border"
-    />
-  )
+export default function C() {
+    return (
+      
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+          <div className="overflow-hidden rounded-lg container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+       
+          <Calendar />
+        </div>
+        </section>
+    )
 }
             
        

b32011c0eec77816aa352797fa10bf928b44e89c
brcelso <129978598+brcelso@users.noreply.github.com>
2023-04-30 22:42:37 +0000

    ok

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -20,15 +20,12 @@ import { Calendar } from "@/components/ui/calendar"
 export default function C() {
     return (
       
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-          <div className="overflow-hidden rounded-lg container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-       
+      
           <Calendar
           mode="single"
           
           className="rounded-md border" />
-        </div>
-        </section>
+       
     )
 }
             

2fe8118abfa9f6d2ac60d08ac0b0cbec4577e07e
brcelso <129978598+brcelso@users.noreply.github.com>
2023-04-30 22:57:23 +0000

    ok

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,21 +1,7 @@
 "use client"
 
 import "@/styles/globals.css"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import Image from "next/image"
-import Link from "next/link"
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-
-import { Icons } from "@/components/icons"
-
-//import { PromoVideo } from "@/components/promo-video"
-
-//import DashboardPage from "@/app/examples/dashboard/page"
-import TradingViewWidget from "@/app/charts/page"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Calendar } from "@/components/ui/calendar"
 
 export default function C() {
